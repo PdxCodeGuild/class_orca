df713045e650a4c42813ef378a91dbc3ff2dded0
sjohnston82 <stephen.johnston1@yahoo.com>
2020-10-06 12:07:40 -0700

    Lab 9 - Unit Conversion

---
+++ code/stephen/lab_09.py
@@ -0,0 +1,66 @@
+print('Welcome to the Unit Converter!')
+
+unit1 = input('''What is the input unit of measure you would like to convert?
+ft for feet, mi for miles, km for kilometer or m for meters:
+>''')
+dist = int(input('''What is the amount of input units you would like to convert?
+>'''))
+unit2 = input('''What is the output unit of measure you would like to convert?
+ft for feet, mi for miles, km for kilometer or m for meters:
+>''')
+while unit1 == 'ft':
+    if unit2 == 'm':
+        total = dist * 0.3048
+        print(f'{dist} {unit1} is {total:.2f}{unit2}s!')
+        break
+    if unit2 == 'mi':
+        total = dist / 5280
+        print(f'{dist} {unit1} is {total:.2f}{unit2}s!')
+        break
+    if unit2 == 'km':
+        total = dist / 3280
+        print(f'{dist} {unit1} is {total:.2f}{unit2}s!')
+        break
+while unit1 == 'm':
+    if unit2 == 'ft':
+        total = dist / 0.3048
+        print(f'{dist} {unit1} is {total:.2f}{unit2}!')
+        break
+    if unit2 == 'mi':
+        total = dist / 1609.34
+        print(f'{dist} {unit1} is {total:.2f}{unit2}s!')
+        break
+    if unit2 == 'km':
+        total = dist / 1000
+        print(f'{dist} {unit1} is {total:.2f}{unit2}s!')
+        break
+while unit1 == 'mi':
+    if unit2 == 'm':
+        total = dist * 1609.34
+        print(f'{dist} {unit1} is {total:.2f}{unit2}s!')
+        break
+    if unit2 == 'ft':
+        total = dist * 5280
+        print(f'{dist} {unit1} is {total:.2f}{unit2}!')
+        break
+    if unit2 == 'km':
+        total = dist * .621371
+        print(f'{dist} {unit1} is {total:.2f}{unit2}s!')
+        break
+while unit1 == 'km':
+    if unit2 == 'm':
+        total = dist * 1000
+        print(f'{dist} {unit1} is {total:.2f}{unit2}s!')
+        break
+    if unit2 == 'ft':
+        total = dist * 3280
+        print(f'{dist} {unit1} is {total:.2f}{unit2}!')
+        break
+    if unit2 == 'mi':
+        total = dist * .621371
+        print(f'{dist} {unit1} is {total:.2f}{unit2}s!')
+        break
+    
+
+
+    
