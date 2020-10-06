96d33784f4635bd4e4dea775330ea86d0c54d5c5
Quinton Baseman <quintonbaseman@Quintons-MacBook-Air.local>
2020-10-06 13:58:11 -0700

    lab09.py

--- code/quinton/lab09.py
+++ code/quinton/lab09.py
@@ -3,31 +3,37 @@ input_units = input('what are the input units?\n>')
 output_units = input('what are the output units?\n>')
 answer = 0
 final = 0
+keys = ['ft', 'm', 'mi', 'km', 'yd', 'in']
 
-if input_units == 'ft':
-    answer += start * .3048
-elif input_units == 'm':
-    answer += start * 1
-elif input_units == 'mi':
-    answer += start * 1609.34
-elif input_units == 'km':
-    answer += start * 1000
-elif input_units == 'yd':
-    answer += start * .9144
-elif input_units == 'in':
-    answer += start * .0254
-
-if output_units == 'mi':
-    final += answer / 1609.34
-elif output_units == 'km':
-    final += answer / 1000
-elif output_units == 'm':
-    final += answer
-elif output_units == 'ft':
-    final += answer / .3048
-elif output_units == 'yd':
-    final += answer / .9144
-elif output_units == 'in':
-    final += answer / .0254
-
-print(f'{start} {input_units} is {final} {output_units}')
+# checking if the input and output are in the keys list
+if keys.count(input_units) > 0 and keys.count(output_units) > 0:
+    # if statement for input
+    if input_units == 'ft':
+        answer += start * .3048
+    elif input_units == 'm':
+        answer += start * 1
+    elif input_units == 'mi':
+        answer += start * 1609.34
+    elif input_units == 'km':
+        answer += start * 1000
+    elif input_units == 'yd':
+        answer += start * .9144
+    elif input_units == 'in':
+        answer += start * .0254
+    # if statement for output
+    if output_units == 'mi':
+        final += answer / 1609.34
+    elif output_units == 'km':
+        final += answer / 1000
+    elif output_units == 'm':
+        final += answer
+    elif output_units == 'ft':
+        final += answer / .3048
+    elif output_units == 'yd':
+        final += answer / .9144
+    elif output_units == 'in':
+        final += answer / .0254
+    
+    print(f'{start} {input_units} is {final} {output_units}')
+else:
+    print('invalid units')
