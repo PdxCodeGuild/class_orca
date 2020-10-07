077d66c647561329fa42dce5a80edc38d1aa3da2
johnfial <12399190+johnfial@users.noreply.github.com>
2020-10-06 17:00:55 -0700

    first commit

---
+++ code/john/day2.py
@@ -0,0 +1,11 @@
+print(True == 1)
+
+
+# SPOILER... for lab?
+#truthy and falsy ONLY apply when doing a conditional
+
+fruits = ['apples', 'bananas', 'pears']
+print(fruits)
+
+fruits_tuple = tuple(fruits)
+print(fruits_tuple)
