449201897c30a67a8546861c1ef0a022f7aa1734
johnfial <12399190+johnfial@users.noreply.github.com>
2020-10-06 17:20:37 -0700

    Lab 10

--- code/john/lab10_average_numbers.py
+++ code/john/lab10_average_numbers.py
@@ -4,13 +4,6 @@
 # Lab 10: Average Numbers
 ##############################################################################
 
-# PSEUDOCODE:
-# # # ask for user input
-# add user input to sum of numbers
-# # # when user input == done,...
-# get length of list with len(nums)
-# divide by length of list!
-
 nums = []
 user_input = input('Enter a single number, or "done" when done to return the average: ')
 user_input = float(user_input)
@@ -20,21 +13,21 @@ while user_input != 'done':
     user_input = float(user_input)
     nums.append(user_input)
     numbers_sum += user_input
-    print(nums) # REMOVE
+    # print(nums) # REMOVE
     user_input = input('Enter another, or "done" when done to return the average: ')
     continue
 else:
-    print(nums) # REMOVE
-    print(f'There are {len(nums)} items in the list.') # REMOVE
+    # print(nums) # REMOVE
+    print(f'There are {len(nums)} numbers in the list {nums}.') # REMOVE
     print(f'The sum total is {numbers_sum}.') # REMOVE
-    print('WHILE LOOP TERMINATION') # REMOVE
-
-# for num in nums:
-#     print(num)
-#     print(type(num))
-#     print(numbers_sum)
+    average = numbers_sum / len(nums)
+    print(f'The average of the list is {average}.')
+    # print('WHILE LOOP TERMINATION') # REMOVE
 
 
-# # loop over the indices
-# for i in range(len(nums)):
-#     print(nums[i])
+# PSEUDOCODE:
+# # # ask for user input
+# add user input to sum of numbers
+# # # when user input == done,...
+# get length of list with len(nums)
+# divide by length of list!
