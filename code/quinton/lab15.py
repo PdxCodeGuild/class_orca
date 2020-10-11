a327d3dcd5184f0a52af6fbb67fca759676db1f3
Quinton Baseman <quintonbaseman@Quintons-MacBook-Air.local>
2020-10-10 19:51:27 -0700

    v2

--- code/quinton/lab15.py
+++ code/quinton/lab15.py
@@ -1,27 +1,15 @@
 # Welcome to lab15 by Quinton Baseman
 
-# dictionary of single numbers plus teens
-single = {
-    0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four',
-    5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
-    10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen',
-    14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen',
-    18: 'eighteen', 19: 'nineteen'
-}
-# dictionary of multiples of ten
-tens = {
-    0: 'zero', 2: 'twenty', 3: 'thirty', 4: 'forty', 5: 'fifty',
-    6: 'sixty', 7: 'seventy', 8: 'eighty', 9: 'ninety'
-}
-
-hundreds = {
-    1: 'one hundred', 2: 'two hundred', 3: 'three hundred',
-    4: 'four hundred', 5: 'five hundred', 6: 'six hundred',
-    7: 'seven hundred', 8: 'eight hundred', 9: 'nine hundred'
-}
-
-# getting input from user, module the number to get the 'ones' digit and 'tens' digit
-x = int(input('enter number between 0-999\n>'))
+# getting the input from the user
+x = int(input('Enter a number between 0-999\n>'))
+# key value pairs for ones, tens, hundreds digit. plus extra dictionary for 10-19
+ones = {0: 'Zero', 1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine'}
+weirds = {10: 'Ten', 11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', 15: 'Fifteen',
+          16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
+tens = {2: 'Twenty', 3: 'Thirty', 4: 'Forty', 5: 'Fifty', 6: 'Sixty', 7: 'Seventy', 8: 'Eighty', 9: 'Ninety'}
+hundreds = {1: 'One Hundred', 2: 'Two Hundred', 3: 'Three Hundred', 4: 'Four Hundred', 5: 'Five Hundred',
+            6: 'Six Hundred', 7: 'Seven Hundred', 8: 'Eight Hundred', 9: 'Nine Hundred'}
+# manipulating digits to determine which dictionaries to use
 ones_digit = x % 10
 if x < 99:
     tens_digit = x // 10
@@ -29,42 +17,59 @@ else:
     tens_digit = int((x/10) % 10)
 hundreds_digit = x // 100
 
-# converting and returning the singles digit as a string
-def convert_single(blah):
-    for key in single:
+# converting the one digit
+def convert_one(blah):
+    for key in ones:
         if blah == key:
-            return single[key]
+            return ones[key]
 
-# converting and returning the tens digit as a string
+# converting the ten digit
 def convert_ten(blah):
     for key in tens:
         if blah == key:
             return tens[key]
 
-# converting and returning the hundreds digit as a string
+# converting the hundred digit
 def convert_hundred(blah):
     for key in hundreds:
         if blah == key:
             return hundreds[key]
 
-# if x is already in the single dictionary, it will just print
-for i in single:
-    if x == i:
-        print(single[i])
+# checking if the digit is in the weird dictionary
+def weird_digit(blah):
+    if blah > 99:
+        if 10 <= blah % 100 <= 19:
+            i = blah % 100
+            return f'{hundreds[hundreds_digit]} {weirds[i]}'
+        else:
+            return False
 
-# checking all numbers over 19 and converting to strings using above functions
-if x > 19:
-    if convert_hundred(hundreds_digit):
-        if convert_ten(tens_digit) != 'zero' and convert_single(ones_digit) != 'zero':
-            print(f'{convert_hundred(hundreds_digit)}-{convert_ten(tens_digit)}-{convert_single(ones_digit)}')
-        elif convert_ten(tens_digit) != 'zero' and convert_single(ones_digit) == 'zero':
-            print(f'{convert_hundred(hundreds_digit)}-{convert_ten(tens_digit)}')
-        elif convert_ten(tens_digit) == 'zero' and convert_single(ones_digit) != 'zero':
-            print(f'{convert_hundred(hundreds_digit)}-{convert_single(ones_digit)}')
-        elif convert_ten(tens_digit) == 'zero' and convert_single(ones_digit) == 'zero':
-            print(f'{convert_hundred(hundreds_digit)}')
+# if number is in the weird dictionary, it will print
+if 10 <= x <= 19:
+    for key in weirds:
+        if x == key:
+            print(weirds[key])
+# checking which dictionary combo is needed for the correct output
+else:
+    if not weird_digit(x):
+        if convert_hundred(hundreds_digit):
+            if convert_ten(tens_digit):
+                if convert_one(ones_digit) != 'Zero':
+                    print(hundreds[hundreds_digit], tens[tens_digit], ones[ones_digit])
+                else:
+                    print(hundreds[hundreds_digit], tens[tens_digit])
+            else:
+                if convert_one(ones_digit) != 'Zero':
+                    print(hundreds[hundreds_digit], ones[ones_digit])
+                else:
+                    print(hundreds[hundreds_digit])
+        else:
+            if convert_ten(tens_digit):
+                if convert_one(ones_digit) != 'Zero':
+                    print(tens[tens_digit], ones[ones_digit])
+                else:
+                    print(tens[tens_digit])
+            else:
+                print(ones[ones_digit])
     else:
-        if convert_ten(tens_digit) != 'zero' and convert_single(ones_digit) != 'zero':
-            print(f'{convert_ten(tens_digit)}-{convert_single(ones_digit)}')
-        elif convert_ten(tens_digit) != 'zero' and convert_single(ones_digit) == 'zero':
-            print(f'{convert_ten(tens_digit)}')
+        print(weird_digit(x))
