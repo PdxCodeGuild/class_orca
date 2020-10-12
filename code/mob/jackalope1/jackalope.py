4f03eb2760d94f9a8960803e3b7b02270ffba934
a-garey <agarey@uchicago.edu>
2020-10-12 13:55:39 -0400

    jackalope lab 1

---
+++ code/mob/jackalope1/jackalope.py
@@ -0,0 +1,25 @@
+population = [0, 0] 
+
+#loop 1: increment age by one
+    #
+    #if the age is over 10: pop
+    #if thee age 4-8: 
+        #add two element value to the list
+    # each year = one loop, counter 
+
+def baby_jacks(population):
+    year = 0
+    while year <= 10:
+        for i in range (0, len(population), 1):
+            population[i] += 1
+        for 
+
+        # if population[i] in range(4, 8, 1):
+        #     population = [population + [0,0]]
+    
+        year += 1
+        
+        print(year)
+        print(population)
+
+baby_jacks(population)
