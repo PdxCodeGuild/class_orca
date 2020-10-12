5dba4403023baa8151ad427e4245fd327d559ff4
johnfial <12399190+johnfial@users.noreply.github.com>
2020-10-12 10:54:13 -0700

    push 11am mon

--- code/mob/jackalope4/jackalope.py
+++ code/mob/jackalope4/jackalope.py
@@ -5,18 +5,34 @@
 # every year + 2 jacks
 
 
-population = [0,1]    # jackalope population
+population = [0,0]    # jackalope population
 year = 0    # goes up 1 
 
 while len(population) < 11:
-    for jack in population:
-        population.insert(jack, + 1)
-        print(f'{jack} year old jack')
+    print(f'len(population) is {len(population)}')
+    # Main code to count adults and add babies
     # for x in pop > 3 and pop < 9:
     #     pass
-    population.append(0) # 2 babies
-    population.append(0)
 
-    print(f'{population} is pop')
+    # population.append(0) # 2 babies
+
+    # print(f'{population} is pop')
+
+
+    # Increase age by 1 for each jack in population
     year += 1
-    print(f'{year} is year')
+    population.append(0) # Temporary add a baby
+
+    for age in population:
+        print(f'Line 27 for a {age}-year old jack')
+        new_age = age
+        population.remove(age)
+        new_age += 1
+        population.append(new_age)
+        print(f'Line 32 for a {age}-year old jack')
+        print(population)
+
+    print(f'{year} is year, end of while loop...')
+
+print(population)
+print('end')
