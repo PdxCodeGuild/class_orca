92c3b6e48f5f8583b3b3f4e572180891e94e9045
bit_jax <axel.omley@gmail.com>
2020-10-12 12:35:47 -0700

    update

--- code/mob/jackalope2/jackalope_lab_ver1.py
+++ code/mob/jackalope2/jackalope_lab_ver1.py
@@ -10,23 +10,22 @@ def main():
     def age():
 
         fertile = 0
+        while len(population) <= 1000:
+        # append population with fertile
         for i, age in enumerate(population):
-            # if age >= 4 and age <= 8:
-            #     fertile += 1
-            if age == 4:
+        for i in range(len(population)):
+            if 4 <= population[i] <= 8:
                 fertile += 1
-            if age == 8:
-                fertile -= 1
-            population[i] += 1
+
+            # 1 fertile removed at 8
+            # 1 population  removed at 10
             #     population.append(0)
             #     population.append(0)
-        # return fertile
+        return fertile
         print(fertile)
         print(population)
-    
-    for i in range(len(population)):
-        if 4 <= population[i] <= 8:
-            fertile = True
+
+
     
 
 
@@ -54,7 +53,13 @@ def main():
 
     # print(jackaloupe_breeding_mach(2))
     
-
+            # if age >= 4 and age <= 8:
+            #     fertile += 1
+            # if age == 4:
+            #     fertile += 1
+            # if age == 8:
+            #     fertile -= 1
+            # population[i] += 1
 
 
 
