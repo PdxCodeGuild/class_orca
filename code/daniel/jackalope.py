5fb2efada6420d6516e1d4315f8a17a400c47a6b
Dan Eggimann <daneggimann@eggxact.local>
2020-10-12 14:13:56 -0700

    version 2

--- code/daniel/jackalope.py
+++ code/daniel/jackalope.py
@@ -1,33 +1,62 @@
-# append(population())
-# while loop refernceing population item sum 
+# '''
+# PDX Code Guild
+# Daniel Eggimann & Quinton Baseman
+# Jackalope Lab
+# '''
+# jackals = [0, 0]
+# years = 0
 
-jackals = [0, 0]
-population = len(jackals)
+# while len(jackals) <= 1000:
+#     for num in range(len(jackals)):
+        
+#         # living jackals
+#         if jackals[num] <= 9:
+#             # age the jackal by one year
+#             jackals[num] = jackals[num] + 1
+#             # # birthing jackals
+#             if 4 <= jackals[num] <= 8:
+#                 jackals.append(0)
+
+#         # removing dead jackals
+#         elif jackals[num] == 10:
+#             jackals.remove(jackals[num])
+    
+#     # time pass
+#     years += 1
+    
+
+# print(years)
+# print(len(jackals))
+
+jackals = [
+    {
+    'name': 'jackalope_1',
+    'age': 0,
+    'sex': '',
+    'pregnant': ''
+    }
+]
 years = 0
 
-# add years to jackals
-while population <= 1000:
-    for num in range(0, population):
-        if num <= 10:
-            jackals[num] = jackals[num] + 1
-        elif num >10:
-            jackals.remove(num)
-    print(jackals)
+while len(jackals) <= 1000:
+    for dictionary in range(len(jackals)):
+        # living jackals
+        if jackals[dictionary['age']] <= 9:
+            # 'age' the jackal by one year
+            jackals[dictionary['age']] = jackals[dictionary['age']] + 1
+            # # birthing jackals
+            if 4 <= jackals[dictionary['age']] <= 8:
+                jackals.append(0)
 
+        # removing dead jackals
+        elif jackals[dictionary['age']] == 10:
+            jackals.remove(jackals[dictionary['age']])
+    
+    # time pass
+    years += 1
+    
 
-# adding new born jackal
-counter = 0
-while population <= 1000:
-    counter += 1
-    for item in jackals:
-        if 3 < item <= 10:
-            jackals.append(0)
-        
-print(population)
+print(years)
+print(len(jackals))
 
-
-# def main():
-#     # age the population
-    # append the population
-    # remove deaths
 
