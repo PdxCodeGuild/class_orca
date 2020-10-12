781f8616fe6019ee69e5248d4bb06d9f480c06a4
bit_jax <axel.omley@gmail.com>
2020-10-12 14:28:05 -0700

    working version

--- code/mob/jackalope2/jackalope_lab_ver1_jo.py
+++ code/mob/jackalope2/jackalope_lab_ver1_jo.py
@@ -6,39 +6,30 @@ population = [0,0]
 
 def main():
 
-
-    def age():
-        fertile = 0
+    years = 0
+    def age(years):
         global population
         # appends list with 0 for new jackalopes (fertile) 
         while len(population) <= 1000:
-            for x in range(fertile):
-                population = population.append(0)
-            # ages all jackalopes by one year (adds 1 to each element of list)    
-            population = [x + 1 for x in range(len(population))]
-            # finds total number of fertile rabbits and adds to fertile
-            for i in range(len(population)):
-                fertile = 0
-                if 4 <= population[i] <= 8:
+            print(years)
+            years += 1
+            fertile = 0
+            for i in population:
+                if i >= 4 and i <=8:
                     fertile += 1
-            # removes 10 year old rabbits from list
-            population = [population.rem(x) for x in population if x == 10]
-
-            
-
-            # 1 fertile removed at 8
-            # 1 population  removed at 10
-            #     population.append(0)
-            #     population.append(0)
-        return fertile
-        print(fertile)
-        print(population)
+            for x in range(0,fertile//2):
+                population.append(0)
+                population.append(0)
+            # ages all jackalopes by one year (adds 1 to each element of list)    
+            population = [x + 1 for x in population]
+            # finds total number of fertile rabbits and adds to fertile
+            population = [x for x in population if x != 10]
+        print(years)
+        return years
 
 
-    
-
-
-    age()
+    total_years = age(years)
+    print(f'It takes {total_years} years to make 1000 jackalopes')
 
 main()
 
