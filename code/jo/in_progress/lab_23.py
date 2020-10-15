649a5da48202470adccf19d18b420e5e372c266d
bit_jax <axel.omley@gmail.com>
2020-10-15 16:23:27 -0700

    lab 23 progress

---
+++ code/jo/in_progress/lab_23.py
@@ -0,0 +1,91 @@
+""" 
+Still to do:
+fix c
+ver 3
+functions
+ """
+
+
+# opens csv and splits each line into a list element
+with open('contacts.csv', 'r') as file:
+    lines = file.read().split('\n')
+
+
+# list of contacts. will be list of dicts
+contacts = []
+
+# makes each element of lines list into its own list
+for i in lines:
+    contacts.append(i.split(","))
+
+# makes first line of csv it's own list then removes from contacts list. will be used as key in final dicts
+key1 = contacts[0]
+del contacts[0]
+
+# makes each contact a list of key/value tuples
+contacts_tup_list = []
+for i in contacts:
+    entry = list(zip(key1,i))
+    contacts_tup_list.append(entry)
+
+# turns list of tuples to list of dicts
+contacts_list = []
+for i in contacts_tup_list:
+    contacts_list.append(dict(i))
+
+crud = input("\nWhat would you like to do?\nCreate a new record? enter 'c' \nRetrieve a record? enter 'r' \nUpdate a record? enter 'u' \nDelete a record? enter 'd' \nexit? enter 'e' \n")
+while True:
+    if crud == 'c':
+        name = input("What is the agent's name? ")
+        town = input("What is the agent's hometown? ")
+        handle = input("What is the agent's handle? ")
+        specialty = input("What is the agent's specialty")
+        new_contact = [name,town,handle,specialty]
+        contacts.append(new_contact)
+        # make dict-combining function here
+        print(contacts_list)
+        crud = input("\nWhat would you like to do?\nCreate a new record? enter 'c' \nRetrieve a record? enter 'r' \nUpdate a record? enter 'u' \nDelete a record? enter 'd' \nexit? enter 'e' \n")
+
+    elif crud == 'r':
+        agent = input("Whose record would you like to view? ")
+        request = []
+        for i in contacts_list:
+            if i["name"] == agent:
+                request.append(i)
+        if request:
+            print(request)
+        else:
+            print('There were no records with that name')
+        crud = input("\nWhat would you like to do?\nCreate a new record? enter 'c' \nRetrieve a record? enter 'r' \nUpdate a record? enter 'u' \nDelete a record? enter 'd' \nexit? enter 'e' \n")
+        
+    elif crud == 'u':
+        name = input("Whose record would you like to update? ")
+        change = input("What would you like to update? (name,hometown,handle,spcialty) ")
+        update = input(f"What would you like to change agent's {change} to? ")
+        for i in contacts_list:
+            if i["name"] == name:
+                i[f'{change}'] = update 
+                print(f"The new record is: {i}")
+        crud = input("\nWhat would you like to do?\nCreate a new record? enter 'c' \nRetrieve a record? enter 'r' \nUpdate a record? enter 'u' \nDelete a record? enter 'd' \nexit? enter 'e' \n")
+
+    elif crud == 'd':
+        record = input("What is the name on the record you would like to delete? ")
+        for i in contacts_list:
+            if i["name"] == record:
+                contacts_list.remove(i)
+                print(f"The record for {record} has been deleted.")
+                print(contacts_list)
+                crud = input("\nWhat would you like to do?\nCreate a new record? enter 'c' \nRetrieve a record? enter 'r' \nUpdate a record? enter 'u' \nDelete a record? enter 'd' \nexit? enter 'e' \n")
+    elif crud == "e":
+        break
+    else:
+        print("\nThat is an invalid entry. Please try again.\n")
+        crud = input("\nWhat would you like to do?\nCreate a new record? enter 'c' \nRetrieve a record? enter 'r' \nUpdate a record? enter 'u' \nDelete a record? enter 'd' \nexit? enter 'e' \n")
+
+
+# # print(lines)
+# # print(key1)
+# # print(contacts)
+# # print(contacts_tup_list)
+# print(contacts_list)
+
