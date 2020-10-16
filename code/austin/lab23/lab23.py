73f45e9e1b9f7b5a4509031a1e8bffe79ae60b8f
a-garey <agarey@uchicago.edu>
2020-10-16 15:25:29 -0400

    lab23 v2

--- code/austin/lab23/lab23.py
+++ code/austin/lab23/lab23.py
@@ -43,18 +43,49 @@ def view_record():
             print(record)
     
     edit_db()
-    return find_record
+    return record
 
 #Update record
-def update_record():
+def update_record(): 
     find_record = input("Please enter name ")
     for record in contact_obj:
         if record['name'] == find_record:
-            #Select which field to change
-            #Display input line for that field
-            #Re-display entire record
-            pass
+            select_change = input("What value would you like to change? Choose name, animal, band or tea  ")
+            if select_change == 'name':
+                new_name = input("Name? ")
+                record['name'] = new_name
+            if select_change == 'animal':
+                new_animal = input("Favorite animal? ")
+                record['fav_animal'] = new_animal
+            if select_change == 'band':
+                new_band = input('Favorite band? ')
+                record['fav_band'] = new_band
+            if select_change == 'tea':
+                new_tea = input("Favorite kind of tea? ")
+                record['fav_tea'] = new_tea      
+        print("Updated record: ", record)
+    
+    edit_db()
+    return record
 
+#Delete record
+def delete_record():
+    find_record = input("Please enter name ")
+    for i in range(len(contact_obj)):
+        if contact_obj[i]['name'] == find_record:
+            del contact_obj[i]
+            break
+    print(f'New contact list: {contact_obj}')
+    
+    edit_db()
+    return contact_obj
+
+#Convert dictionary to strings
+def back_to_strings():
+    # separator = ', '
+    # key_string = separator.join(key_list)
+    print(key_list)
+    print(lines)
 
 #CRUD REPL
 def edit_db():
@@ -63,5 +94,22 @@ def edit_db():
         new_record()
     if select == "view":
         view_record()
+    if select == "edit":
+        update_record()
+    if select == "delete":
+        delete_record()
+    if select == "done":
+        back_to_strings()
+        # write_file()
+
 edit_db()
+
+
+
+# #Write new contact list to CSV
+# def write_file():
+#     with open('contacts.csv', 'w') as file:
+#         file.write(contact_obj)
+#     print(contact_obj)
+#     exit()
     
