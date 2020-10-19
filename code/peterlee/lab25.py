f237c0185d00bb933916c28ab14196424dd222ca
unknown <plx1@protonmail.com>
2020-10-19 17:14:45 -0400

    added lab25v2

--- code/peterlee/lab25.py
+++ code/peterlee/lab25.py
@@ -2,12 +2,14 @@ class account:
 
     def __init__(self):
         self.balance = 0
+        self.list = []
 
     def check_balance(self):
         return self.balance
     
     def deposit(self, deposit_amount):
         self.balance += deposit_amount
+        self.list.append(f'User deposited {deposit_amount}.')
         return self.balance
 
     def check_withdrawal(self, check_withdrawal_amount):
@@ -18,4 +20,22 @@ class account:
     
     def withdraw(self, withdrawal_amount):
         self.balance -= withdrawal_amount
+        self.list.append(f'User withdrew {withdrawal_amount}.')
         return self.balance
+
+    def print_transactions(self):
+        print(self.list)
+
+a1 = account()
+
+a1.deposit(500)
+a1.deposit(500)
+a1.deposit(500)
+a1.deposit(500)
+
+a1.withdraw(300)
+a1.withdraw(300)
+a1.withdraw(300)
+a1.withdraw(300)
+
+a1.print_transactions()
