95f6d6bda6cd00ee9067a9db0fa4678922f56cab
bit_jax <axel.omley@gmail.com>
2020-10-20 16:29:35 -0700

    lab 23 & 26 update

--- code/jo/in_progress/lab_23.py
+++ code/jo/in_progress/lab_23.py
@@ -1,7 +1,8 @@
 """ 
 Still to do:
+
+add view all
 fix c
-ver 3
 functions
  """
 
@@ -10,42 +11,63 @@ functions
 with open('contacts.csv', 'r') as file:
     lines = file.read().split('\n')
 
-
-
 contacts = []
 contacts_tup_list = []
 contacts_list = []
 
-# makes each element of lines list into its own list
-for i in lines:
-    contacts.append(i.split(","))
+def make_contacts():
+    # makes each element of lines list into its own list
+    for i in lines:
+        contacts.append(i.split(","))
 
-# makes first line of csv it's own list then removes from contacts list. will be used as key in final dicts
-key1 = contacts[0]
-del contacts[0]
+    # makes first line of csv it's own list then removes from contacts list. will be used as key in final dicts
+    key1 = contacts.pop()
 
-# makes each contact a list of key/value tuples
-for i in contacts:
-    entry = list(zip(key1,i))
-    contacts_tup_list.append(entry)
+    # makes each contact a list of key/value tuples
+    for i in contacts:
+        entry = list(zip(key1,i))
+        contacts_tup_list.append(entry)
 
-# turns list of tuples to list of dicts
-for i in contacts_tup_list:
-    contacts_list.append(dict(i))
+    # turns list of tuples to list of dicts
+    for i in contacts_tup_list:
+        contacts_list.append(dict(i))
+    return contacts_list
+
+
+
+def add_contact(new_contact):
+        # makes each element of lines list into its own list
+    for i in lines:
+        contacts.append(i.split(","))
+
+    # makes first line of csv it's own list then removes from contacts list. will be used as key in final dicts
+    key1 = contacts.pop()
+    contacts.append(new_contact)
+    # makes each contact a list of key/value tuples
+    for i in contacts:
+        entry = list(zip(key1,i))
+        contacts_tup_list.append(entry)
+
+    # turns list of tuples to list of dicts
+    for i in contacts_tup_list:
+        contacts_list.append(dict(i))
+    return contacts_list
+
+
+contacts_list = make_contacts()
 
 crud = input("\nWhat would you like to do?\nCreate a new record? enter 'c' \nRetrieve a record? enter 'r' \nUpdate a record? enter 'u' \nDelete a record? enter 'd' \nexit? enter 'e' \n")
 while True:
     if crud == 'c':
         name = input("What is the agent's name? ")
-        town = input("What is the agent's hometown? ")
+        town = input("What is the agent's ship? ")
         handle = input("What is the agent's handle? ")
-        specialty = input("What is the agent's specialty")
+        specialty = input("What is the agent's specialty? ")
         new_contact = [name,town,handle,specialty]
-        contacts.append(new_contact)
-        # make dict-combining function here
+        # contacts.append(new_contact)
+        contacts_list = add_contact(new_contact)
         print(contacts_list)
         crud = input("\nWhat would you like to do?\nCreate a new record? enter 'c' \nRetrieve a record? enter 'r' \nUpdate a record? enter 'u' \nDelete a record? enter 'd' \nexit? enter 'e' \n")
-
     elif crud == 'r':
         agent = input("Whose record would you like to view? ")
         request = []
@@ -70,6 +92,7 @@ while True:
 
     elif crud == 'd':
         record = input("What is the name on the record you would like to delete? ")
+        
         for i in contacts_list:
             if i["name"] == record:
                 contacts_list.remove(i)
@@ -89,5 +112,45 @@ while True:
 # # print(contacts_tup_list)
 # print(contacts_list)
 
-with open('contacts.csv', 'r') as file:
-    file.write(str(contacts_list))
+
+#--------------------------------------------------------
+# reverses the csv to list of dictionaries process to write to contacts.csv
+
+# turns list of dictionaries to list of tuples
+contacts = []
+for d in contacts_list:
+    contact = [(k,v) for k,v in d.items()]
+    contacts.append(contact)
+
+# unzips the tuples into lists
+uz_contacts = []
+for i in contacts:
+    uz_contact = list(zip(*i))
+    uz_contacts.append(uz_contact)
+
+# separates the key element and contacts within each contact list and recombines into one list
+new_csv = []
+for l in uz_contacts:
+    for i in l:
+        key = l[0]
+        contact = l[1]
+    new_csv.append(contact)
+new_csv.insert(0,key)
+
+# turns the list into a string and formats back to original contacts.csv format.
+new_csv = [','.join(i) for i in new_csv]
+new_csv = str(new_csv).strip('[]')
+new_csv = new_csv.replace(" ",'')
+new_csv = new_csv.replace("','",'\n')
+new_csv = new_csv.strip("'")
+
+
+
+
+with open('contacts_out.csv', 'w') as file:
+    file.write(str(new_csv))
+
+
+
+
+
