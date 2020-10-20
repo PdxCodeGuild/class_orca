de6ffe359c09b4f96a58a649bd50805deb5be962
nico-252 <applejack_@live.com>
2020-10-19 19:04:57 -0700

    Finshed Lab 25

--- code/tony/lab25.py
+++ code/tony/lab25.py
@@ -2,41 +2,47 @@ class ATM:
     def __init__(self):
         self.bal = 50
         self.check = False
+        self.transactions = []
     def check_balance(self):
         return self.bal
 
     def deposit(self,amount):
         self.bal = self.bal + amount
+        self.transactions.append(f" desposited ${amount}") 
         return self.bal
 
     def withdraw(self,amount):
         if self.check == True:
             self.bal = self.bal - amount
+            self.transactions.append(f" withdrew ${amount}")
             return self.bal
         else:
             pass
 
     def check_withdraw(self,amount):
         if self.bal < amount:
-            print("I'm sorry, Dave. I'm afraid I can't do that.\n")
+            print("\nI'm sorry, Dave. I'm afraid I can't do that.\n")
             self.check = False
             return self.check
         else:
-            print('kk')
+            print("\nkk, money's in ur pocket\n")
             self.check = True
             return self.check
-def again():
-    choices = input('you done? \n1. yea \n2. nah  ')
-    if choices == 1:
-        choices = 4
-        return choices
-    elif choices == 2:
-        choices = 5
+    def print_transactions(self):
+        print(f"\nhere's what u did this time: u{(', then'.join(self.transactions))}\n")
+
+# def again():
+#     choices = input('you done? \n1. yea \n2. nah  ')
+#     if choices == 1:
+#         choices = 4
+#         return choices
+#     elif choices == 2:
+#         choices = 5
         
 bank1 = ATM()
-balance = bank1.check_balance()
+balance = f'${bank1.check_balance()}'
 while True:
-    choices = input('ay. u wanna: \n1. check balance \n2. deposit \n3. withdraw \n4. dip\n')
+    choices = input('ay. u wanna: \n1. check balance \n2. deposit \n3. withdraw \n4. transaction history\n5. dip\n')
     try:
         choices = int(choices)
     except ValueError:
@@ -44,23 +50,29 @@ while True:
         choices = 0
 
     if choices == 1:
-        print(f'aight, u got {balance} in the bank rn. \n')
+        if balance == 0:
+            print('\nu got NOTHIN in the bank. \n')
+        else:
+            print(f'\naight, u got ${balance} in the bank rn. \n')
         # again()
 
     elif choices == 2:
-        amt = float(input('k, how much  '))
+        amt = float(input('\nk, how much  ').strip('$'))
         bank1.deposit(amt)
         balance = bank1.check_balance()
-        print(f'cool, u got {balance} in the bank now.\n')
+        print(f'cool, u got ${balance} in the bank now.\n')
 
     elif choices == 3:
-        amt = float(input('k, how much u finna take?  '))
+        amt = float(input('\nk, how much u finna take?  ').strip('$'))
         bank1.check_withdraw(amt)
         bank1.withdraw(amt)
     
     elif choices == 4:
+        bank1.print_transactions()
+
+    elif choices == 5:
         break
     
     balance = bank1.check_balance()
 
-print('cool.  peace.')
+print('\ncool.  peace.')
