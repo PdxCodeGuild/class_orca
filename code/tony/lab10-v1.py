cd87cfd68609bd61b501c8c548739367c3485fbf
nico-252 <applejack_@live.com>
2020-10-19 18:51:06 -0700

    added Lab 10

---
+++ code/tony/lab10-v1.py
@@ -0,0 +1,12 @@
+# v1
+# ---------
+nums = [5, 0, 8, 3, 4, 1, 6]
+for L in nums:
+    print(L)
+list_total = len(list(nums))
+sum_total = sum(list(nums))
+print(list_total)
+print(sum_total)
+
+average = sum_total / list_total
+print(average)
