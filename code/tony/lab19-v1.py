a4a931c4547ca47e18174099cbd139d67724ad96
nico-252 <applejack_@live.com>
2020-10-21 17:50:32 -0700

    Completed lab 19

---
+++ code/tony/lab19-v1.py
@@ -0,0 +1,31 @@
+import random
+
+first_card = input("What is your first card?  ")
+
+second_card = input("What is your second card?  ")
+
+third_card = input("What is your third card?  ")
+
+player_hand = [first_card, second_card, third_card]
+counted_hand = []
+
+for x in player_hand:
+    if x in ('K','k','Q','q','J','j'):
+        x = 10
+        counted_hand.append(int(x))
+    elif x in ('A','a'):
+            x = 1
+            counted_hand.append(int(x))
+    else:
+        counted_hand.append(int(x))
+
+print(sum(counted_hand))
+
+if sum(counted_hand) > 21:
+    print('Bust')
+elif sum(counted_hand) == 21:
+    print('Blackjack!')
+elif sum(counted_hand) >= 17:
+    print('Stay!')
+else:
+    print('Hit!')
