c07e2b2a75b77f5e6e378ed1178100132c48d232
sjohnston82 <stephen.johnston1@yahoo.com>
2020-10-26 16:27:04 -0700

    Webscraper and tracker updater gui

--- code/stephen/Amazon Webscraper/webscraper copy.py
+++ code/stephen/Amazon Webscraper/webscraper copy.py
@@ -1,6 +1,6 @@
 '''
 Amazon Webscraper!
-The purpose of this program is to scrape relevant data from amazon product url's in a csv.
+The purpose of this program is to scrape relevant data from amazon product urls in a csv.
 Inside the csv is also a buy below threshold, which will print an alert as well as send an
 email notifying the recipient that the price is now below the threshold.
 There is a scheduled task in Windows that runs the script twice daily.'''
@@ -15,6 +15,7 @@ import pandas as pd
 import ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from twilio.rest import Client
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
 
@@ -33,16 +34,18 @@ def amazon_scraper(loops_count=1):
                 price = float(soup.find(id='priceblock_ourprice').get_text().replace('$', '').replace(',', '').strip())
             except:
                 price = ''
-            print(f'{title} -- ${price:.2f}')
+            above = price - tracker.buy_below[x]
+            print(f'{title} -- Price: ${price:.2f} \nThis is ${above:.2f} above the buy below threshold.\n')
             loops += 1
 
-            if price < tracker.buy_below[x]:
+            if price <= tracker.buy_below[x]:
                 print(f'\n{tracker.code[x]} is below purchase threshold of ${tracker.buy_below[x]}\n')
-                send_email(tracker.code[x], tracker.buy_below[x])
-            sleep(5)
+                send_email(tracker.code[x], tracker.buy_below[x], tracker.url[x])
+                
+            # sleep(5)
 
-def send_email(item, threshold):
-    port = 587  # For starttls
+def send_email(item, threshold, url):
+    port = 587
     smtp_server = "smtp.gmail.com"
     sender_email = "awstest82@gmail.com"
     receiver_email = "mossy82nd@gmail.com"
@@ -50,12 +53,15 @@ def send_email(item, threshold):
     message = f"""
 Subject: DEAL ALERT
 
-{item} is below purchase threshold of ${threshold}!"""
+{item} is below purchase threshold of ${threshold}!
+You can purchase it at: {url})"""
 
     context = ssl.create_default_context()
     with smtplib.SMTP(smtp_server, port) as server:
         server.starttls(context=context)
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
+
+
 
 amazon_scraper()
