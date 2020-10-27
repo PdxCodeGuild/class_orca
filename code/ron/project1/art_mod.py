b64f292b6acfb8a5f7791d2fa321c8bb8f215cf2
mansolil <31713362+mansolil@users.noreply.github.com>
2020-10-26 22:49:46 -0700

    Project

--- code/ron/project1/art_mod.py
+++ code/ron/project1/art_mod.py
@@ -22,69 +22,20 @@ import re
 import os
 
 
-#----Dictionary/Lists-------------------------------------------------
-
-# class Art_viewer:
-#    def __init__(self, artist):
-#       self.artist = artist    
-
-
 #----Classes--------------------------------------------------------
 
-
-
+class Art_viewer:
+   def __init__(self, artist):
+      self.artist = artist    
 
 
 #----Global variables, lists, dictionaries--------------------------
 
 
-
-# # Initialize window
-# root = tk.Tk()
-# # root.geometry('475x475')
-# root.resizable(1,1)
-# # root.iconbitmap('Put in the filename')
-
-# welcome_msg = "Welcome!\nPlease select an Artist from the drop down menu,\n and scroll through the master's art!"
-
-# img1 = ImageTk.PhotoImage(Image.open("data/art_images/Albrecht_Dürer_17.jpg"))
+welcome_msg = "Welcome!\nPlease select an Artist from the drop down menu,\n and scroll through the master's art!"
 
 
+#----Functions------------------------------------------------------
 
-# # img2 = tk.ImageTk.PhotoImage(Image.open("data/art_images/Albrecht_Dürer_7.jpg"))
-# # img3 = tk.ImageTk.PhotoImage(Image.open("data/art_images/Albrecht_Dürer_27.jpg"))
-# # img4 = tk.ImageTk.PhotoImage(Image.open("data/art_images/Albrecht_Dürer_57.jpg"))
-# # img5 = tk.ImageTk.PhotoImage(Image.open("data/art_images/Albrecht_Dürer_77.jpg"))
-
-# image_list = [img1]
-
-# # img2, img3, img4, img5]
-
-
-# #----Functions------------------------------------------------------
-
-# print(image_list)
-
-# root.mainloop()
-
-#----Main-----------------------------------------------------------
-
-# artists = ["Vincent_Van Gogh_123.jpg", "some other fuck", "Vincent_Van Gogh_125.jpg","Vincent_Van Gogh_126.jpg","Vincent_Van Gogh_127.jpg", "some fuck"]
-
-name = "Monet"
-
-dir_list = os.listdir('./data/art_images_copy')
-artists = list(dir_list)
-current_artist = []
-
-for artist in artists:
-    if re.search('.+_'+ name + '_.+', artist):
-        current_artist.append(artist)
-print(current_artist)
-    # else:
-    #     print(False)
-
-
-
-# print(dir_list)
+   
 
--- code/ron/project1/art_viewer.py
+++ code/ron/project1/art_viewer.py
@@ -1,27 +1,25 @@
 
 # *'-.-'*'-.-'*'-.-'*-.-'*-.-'*-.-'*-.-'*'-.-'*'-.-'*'-.-'*
 #        Project: Python
-# Assignment/Ver: Project_1.0
+# Assignment/Ver: Capstone 1.0
 #         Author: Ron Mansolilli, ron.mansolilli@gmail.com
 #           Date: 10-27-2020
 # *'-.-'*'-.-'*'-.-'*-.-'*-.-'*-.-'*-.-'*'-.-'*'-.-'*'-.-'*
 
 #----Instructions and notes-----------------------------------------
 ''' 
-Project_1.0 - Art Viewer
+Capstone 1.0 - Art Viewer
  
 '''
 #----Modules--------------------------------------------------------
 
-# import art_mod
+import art_mod
 
 from PIL import ImageTk, Image
 import tkinter as tk
 import PIL
-
-#----Dictionary/Lists-------------------------------------------------
-
-
+import re
+import os
 
 #----Global variables, lists, dictionaries--------------------------
 
@@ -29,67 +27,189 @@ import PIL
 
 #----Functions------------------------------------------------------
 
+def build_list(name):
+    ''' Returns file names for 'current artist' based on artist selected '''
+    dir_list = os.listdir('./data/art_images_copy')
+    artists = list(dir_list)
+    current_artist = []
+    for artist in artists:
+        if re.search('.+_'+ name + '_.+', artist):
+            #^ Searhes for last name regardless of other characters
+            current_artist.append(artist)
+    return current_artist
 
+def fwd(img_num):
+    ''' Forward Button functionality '''
+    global button_back
+    global button_fwd
+    global art_image
+
+    art_image.grid_forget()
+    art_image = tk.Label(canvas_1, image = img[img_num - 1])
+    button_fwd = tk.Button(window_1_frame, text = ">>", command = lambda: fwd(img_num + 1))
+    button_back = tk.Button(window_1_frame, text = "<<", command = lambda: back(img_num - 1))
+    
+    if img_num == len(current_artist):
+        button_fwd = tk.Button(window_1_frame, text = ">>", state = tk.DISABLED)
+    
+    art_image.grid(column = 0, row = 0, padx = 0, pady = 0)
+    button_back.grid(column = 0, row = 1, padx = 10, pady = 10)
+    button_fwd.grid(column = 1, row = 1, padx = 10, pady = 10)
+
+def back(img_num):
+    ''' Back Button functionality '''
+
+    global button_back
+    global button_fwd
+    global art_image
+
+    art_image.grid_forget()
+    art_image = tk.Label(canvas_1, image = img[img_num - 1])
+    button_fwd = tk.Button(window_1_frame, text = ">>", command = lambda: fwd(img_num + 1))
+    button_back = tk.Button(window_1_frame, text = "<<", command = lambda: back(img_num - 1))
+    
+    if img_num == 1:
+        button_back = tk.Button(window_1_frame, text = "<<", state = tk.DISABLED)
+    
+    art_image.grid(column = 0, row = 0, padx = 0, pady = 0)
+    button_back.grid(column = 0, row = 1, padx = 10, pady = 10)
+    button_fwd.grid(column = 1, row = 1, padx = 10, pady = 10)
+
+def change_artist(new_artist):
+    ''' Updates artist information, images, and labels '''
+    global current_artist
+    global img
+    global art_image
+
+    art_image.grid_forget()
+    current_artist = []
+    img = []
+    current_artist = build_list(new_artist)
+    for y in current_artist:
+        img.append(ImageTk.PhotoImage(Image.open(f"data/art_images_copy/" + y)))
+        #^ Creates list of new images for current artist selected
+    art_image = tk.Label(canvas_1, image = img[0])
+    art_image.grid(column = 0, row = 0, padx = 0, pady = 0)
+    refesh_labels(new_artist)
+
+def import_data():
+    ''' Imports and scrubs data for artist_data list '''
+    global artist_data
+
+    # Open .csv file
+    with open('./data/artists_test_data.csv', 'r') as file:
+        lines = file.read().split('\n')
+    # Breakout keys into unique list and delete from .csv
+    keys = lines[0].split(',')
+    del lines[0]
+
+    artist_data = []    
+
+    # Splits data into temp list, reconfigures data, builds artist list of dicts 
+    for x in range(len(lines)):
+        info = {}
+        new = []
+        temp = lines[x].split(',')
+        for z in range(5):
+            new.append(temp[z])
+            info[keys[z]] = new[z]
+        artist_data.append(info)
+
+def last_name_list():
+    ''' Returns list of only last times for combo box '''
+    global last_name
+    name = []
+    last_name = []
+
+    for x in artist_data:
+        l = x['name'].split()
+        name.append(l)
+    for x in name:
+        last_name.append(x[-1])
+    #^ reverses name order and creates new list
+    return last_name
 
 #----Main-----------------------------------------------------------
 
-
-# Initialize window
+# Initialize GUI
 root = tk.Tk()
-# root.geometry('475x475')
 root.resizable(1,1)
-# root.iconbitmap('Put in the filename')
 
-img1 = ImageTk.PhotoImage(Image.open("data/art_images/Albrecht_Dürer_17.jpg"))
+''' Initialize program state and declare variables -----------------------'''
+import_data()
+img = []
+options = last_name_list()
+artist =  tk.StringVar()
+artist.set(options[0])
+genre = ""
+nation = ""
+current_artist = build_list(artist.get())
+#^ Current artist is a list of selected artist's images/filenames
+for y in current_artist:
+    img.append(ImageTk.PhotoImage(Image.open(f"data/art_images_copy/" + y)))
+    #^ Creates list of images for current artist selected
 
-
-
-''' Root Information -------------------------------------------------'''
+''' Root Information ----------------------------------------------------------'''
 root.title("Who's Who in the 'Art World' Zoo")
-title = tk.Label(root, text = "test", underline = 100)
-button_exit = tk.Button(root, text = "Exit", command = root.quit)
+title = tk.Label(root, text = art_mod.welcome_msg, bg = "#ADAD85", width = 100)
 
 # Root placement
 title.grid(column = 0, row = 0, columnspan = 2, padx = 5, pady = 5)
-button_exit.grid(column = 0, row = 2, columnspan = 2, padx = 5, pady = 5)
 
-
-''' Window 1 (images) - image and navigation buttons -------------------'''
+''' Window 1 - image and navigation buttons ----------------------------------'''
 window_1_frame = tk.LabelFrame(root, height = 300, width = 200, text = "Artist Images")
 canvas_1 = tk.Canvas(window_1_frame, height = 250, width = 190, bd = 1, bg = "grey", relief = "groove")
-button_back = tk.Button(window_1_frame, text = "<<")
-button_fwd = tk.Button(window_1_frame, text = ">>")
-
-my_label = tk.Label(canvas_1, image = img1)
-my_label.grid(column = 0, row = 0, padx = 0, pady = 0)
+art_image = tk.Label(canvas_1, image = img[0])
+    #^ Image box
+button_back = tk.Button(window_1_frame, text = "<<", state = tk.DISABLED)
+button_fwd = tk.Button(window_1_frame, text = ">>", command = lambda: fwd(2))
 
 # Window 1 placement
 window_1_frame.grid(column = 0, row = 1, padx = 18, pady = 10)
-    # Framework for window (1 col, 3 rows)
+    #^ Framework for window (1 col, 2 rows)
 canvas_1.grid(column = 0, row = 0, columnspan = 2, padx = 10, pady = 10)
 button_back.grid(column = 0, row = 1, padx = 10, pady = 10)
 button_fwd.grid(column = 1, row = 1, padx = 10, pady = 10)
-
+art_image.grid(column = 0, row = 0, padx = 0, pady = 0)
 
 ''' Window 2 (information) - artist selection and associated info ---------------'''
-window_2_frame = tk.LabelFrame(root, height = 300, width = 200, text = "Artist Information")
-artist_name = tk.Listbox(window_2_frame, width = 20, height = 5, bd = 1, relief = "sunken", selectmode = "single")
-artist_type = tk.Label(window_2_frame, width = 20, height = 5, text = "Label 2", bd = 1, relief = "sunken")
-artist_info = tk.Label(window_2_frame, width = 20, height = 5, text = "Label 3", bd = 1, relief = "sunken")
+window_2_frame = tk.LabelFrame(root, height = 300, width = 200, text = "Artist Information", bg = "#ADAD85")
+artist_name = tk.OptionMenu(window_2_frame, artist, *options)
+artist_genre_label = tk.Label(window_2_frame, width = 20, height = 1, text = "Artist Genre", bg = "#ADAD85")
+artist_genre = tk.Label(window_2_frame, width = 20, height = 1, text = genre, bd = 1, relief = "sunken")
+artist_nationality_label = tk.Label(window_2_frame, width = 20, height = 1, text = "Artist Nationality", bg = "#ADAD85")
+artist_nationality = tk.Label(window_2_frame, width = 20, height = 1, text = nation, bd = 1, relief = "sunken")
+button_update = tk.Button(window_2_frame, text = "Update", command = lambda: change_artist(artist.get()))
+button_exit = tk.Button(window_2_frame, text = "Exit", command = root.quit)
 
 # Window 2 placement
-window_2_frame.grid(column = 1, row = 1, padx = 10, pady = 10)
-    # Framework for window (1 col, 3 rows)
-artist_name.grid(column = 1, row = 0, padx = 10, pady = 10)
-artist_type.grid(column = 1, row = 1, padx = 10, pady = 10)
-artist_info.grid(column = 1, row = 2, padx = 10, pady = 10)
+window_2_frame.grid(column = 1, row = 1, padx = 5, pady = 5)
+    #^ Framework for window (1 col, 7 rows)
+artist_name.grid(column = 1, row = 0, padx = 5, pady = 5)
+button_update.grid(column = 1, row = 1, padx = 5, pady = 5)
+artist_genre_label.grid(column = 1, row = 2, padx = 5, pady = 0)
+artist_genre.grid(column = 1, row = 3, padx = 5, pady = 5)
+artist_nationality_label.grid(column = 1, row = 4, padx = 5, pady = 2)
+artist_nationality.grid(column = 1, row = 5, padx = 5, pady = 2)
+button_exit.grid(column = 1, row = 6, padx = 6, pady = 20)
 
+def refesh_labels(artist):
+    ''' Refreshes artist nationality, genre data when artist selected '''
+    global artist_genre
+    global artist_nationality
 
-    
+    print(artist)
+    for y in artist_data:
+        for key, value in y.items():
+            if artist in value:
+                artist_genre = tk.Label(window_2_frame, width = 20, height = 1, text = y['genre'], bd = 1, relief = "sunken")
+                artist_nationality = tk.Label(window_2_frame, width = 20, height = 1, text = y['nationality'], bd = 1, relief = "sunken")
+                    
+    artist_genre.grid(column = 1, row = 3, padx = 5, pady = 5)
+    artist_nationality.grid(column = 1, row = 5, padx = 5, pady = 2)
+    #^ Reintializes labels with new values
 
-
-ht = 200
-wh = 300
+refesh_labels(artist.get())
 
 root.mainloop()
 
