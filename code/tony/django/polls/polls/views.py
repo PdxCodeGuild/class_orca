01b46fa95aa9d054099e4275e183dfd34f0a6451
nico-252 <applejack_@live.com>
2020-11-10 16:29:00 -0800

    "11/10 polls django update"

--- code/tony/django/polls/polls/views.py
+++ code/tony/django/polls/polls/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ', '.join(q.question_text for q in latest_question_list)
+    return HttpResponse(latest_question_list)
+
+def detail(request, question_id):
+    return HttpResponse(f"you're looking at question number {question_id}.")
+
+def results(request, question_id):
+    return HttpResponse(f"you're looking at the resultes of question number {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"you're voting on question number {question_id}.")
