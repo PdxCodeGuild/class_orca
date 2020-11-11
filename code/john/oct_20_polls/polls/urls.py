6cf0a19c3f7ee69e4583d83da52ff1fb36dd82de
johnfial <12399190+johnfial@users.noreply.github.com>
2020-11-10 20:47:40 -0800

    virtualenv update, minor edits on portfolio site

--- code/john/oct_20_polls/polls/urls.py
+++ code/john/oct_20_polls/polls/urls.py
@@ -1,3 +1,21 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [ # this urlpatterns MUST BE EXACT!
+    path('', views.index, name='index'), # path, then function, then NAME of path
+    path('<int:question_id>/', views.detail, name='detail'),
+    # could be str:string here to look for a string URL
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote')
+]
+
+
+
+
+
+
+# COPIED FROM URLS.PY IN PROJECT FOLDER:
 """polls_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -12,11 +30,11 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')), # app name, 
 ]
+"""
