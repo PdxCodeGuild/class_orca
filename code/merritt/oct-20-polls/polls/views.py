57e16cd5ef0866c449173cd812cfef2121e1a09c
Merritt Lawrenson <merrittlawrenson@gmail.com>
2020-11-11 11:35:49 -0800

    index and detail views and templates

--- code/merritt/oct-20-polls/polls/views.py
+++ code/merritt/oct-20-polls/polls/views.py
@@ -1,14 +1,20 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
 from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question number {question_id}.")
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse(f"You're looking at the results of question number {question_id}.")
