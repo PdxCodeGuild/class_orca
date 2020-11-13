93db2ddf650a93cae8a84dbedb737afdd6b5094c
unknown <plx1@protonmail.com>
2020-11-13 18:41:39 -0500

    updated django lab 1

--- code/peterlee/django_labs/lab1/groceryapp/urls.py
+++ code/peterlee/django_labs/lab1/groceryapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'groceryapp'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('submit/', views.submit, name='submit'),
+    path('submitted/', views.submitted, name='submitted')
 ]
--- code/peterlee/django_labs/lab1/groceryapp/views.py
+++ code/peterlee/django_labs/lab1/groceryapp/views.py
@@ -1,4 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from django.utils import timezone
+
+from .models import GroceryItem
 
 def index(request):
-    return HttpResponse("Grocery Index")
+    complete_grocery_list = GroceryItem.objects.all().filter(completion_check=True)
+    incomplete_grocery_list = GroceryItem.objects.all().filter(completion_check=False)
+    context = {'complete_grocery_list': complete_grocery_list, 'incomplete_grocery_list': incomplete_grocery_list}
+    return render(request, 'groceryapp/index.html', context)
+
+def submit(request):
+    if request.method == 'POST':
+        new_entry = GroceryItem(groceryitem_text=request.POST.get('entry'), pub_date=timezone.now())
+        new_entry.save()
+
+        return HttpResponseRedirect(reverse('groceryapp:submitted'))
+
+def submitted(request):
+    return render(request, 'groceryapp/submitted.html')
