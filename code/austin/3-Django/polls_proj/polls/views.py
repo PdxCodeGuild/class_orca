48e88d008c24f26d258409735f015ff8788d6728
a-garey <agarey@uchicago.edu>
2020-11-12 19:32:32 -0500

    Lab01

--- code/austin/3-Django/polls_proj/polls/views.py
+++ code/austin/3-Django/polls_proj/polls/views.py
@@ -2,11 +2,34 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(
+        pub_date__lte = timezone.now()
+        ).order_by("-pub_date")[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(
+        pub_date__lte = timezone.now()
+        )
+        # return Question.objects.order_by('-pub_date')[:5]
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    
 
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
