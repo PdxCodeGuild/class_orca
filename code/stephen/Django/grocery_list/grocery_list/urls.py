6ae92dfcfb224c249b06055c934fb48431cdc044
sjohnston82 <stephen.johnston1@yahoo.com>
2020-11-13 16:57:56 -0800

    grocery list updates

--- code/stephen/Django/grocery_list/grocery_list/urls.py
+++ code/stephen/Django/grocery_list/grocery_list/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import grocerylist
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('grocerylist/', include('grocerylist.urls'))
+    path('grocerylist/', include('grocerylist.urls')),
 ]
--- code/stephen/Django/grocery_list/grocerylist/views.py
+++ code/stephen/Django/grocery_list/grocerylist/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.utils import timezone
 
-def index(reequest):
-    return HttpResponse('ok')
+from .models import Grocerylist
+
+def index(request):
+    current_grocery_list = Grocerylist.objects.filter(completed_boolean=False).order_by('create_date')
+    completed_items = Grocerylist.objects.filter(completed_boolean=True)
+    context = {
+        'current_grocery_list': current_grocery_list,
+        'completed_items': completed_items
+        }
+    return render(request, 'grocerylist/index.html', context)
+
+def add_item(request):
+    grocery_item = request.POST['item']
+    Grocerylist.objects.create(item_text=grocery_item)
+    return HttpResponseRedirect(reverse('grocerylist:index'))
+
