1494a8794bc2996e9fd3f81e5c5ba4f8d3150f8e
unknown <plx1@protonmail.com>
2020-11-17 19:05:54 -0500

    lab2

--- code/peterlee/django_labs/lab2/Urlapp/urls.py
+++ code/peterlee/django_labs/lab2/Urlapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'Urlapp'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('shorten/', views.shorten, name='shorten'),
+    path('results/', views.results, name='results')
 ]
