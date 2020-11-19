b6cffce5734259452ff26ec40643241789bc16f0
bit_jax <axel.omley@gmail.com>
2020-11-18 16:24:49 -0800

    lab2 v2

--- code/jo/django/url_changer/url_changer/urls.py
+++ code/jo/django/url_changer/url_changer/urls.py
@@ -5,9 +5,6 @@ from . import views
 app_name = 'url_changer'
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('add_item/', views.add_item, name='add_item'),
-    # path('edit', views.edit, name="edit"),
     path('convert/', views.convert, name='convert'),
-    path('redirect/', views.redirect, name='redirect'),
-    
+    path('redirect/<str:short_url>/', views.redirect, name='redirect'),
 ]
