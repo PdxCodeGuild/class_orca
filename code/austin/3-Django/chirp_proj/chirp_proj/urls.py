fa7d7fb8ff95e1c45fd439dc4d4dd8b83d8344b7
a-garey <agarey@uchicago.edu>
2020-11-20 19:23:50 -0600

    Lab 03 cont'd

--- code/austin/3-Django/chirp_proj/chirp_proj/urls.py
+++ code/austin/3-Django/chirp_proj/chirp_proj/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('your_app_name_here.urls')),
+    path('', include('chirp_app.urls')),
 ]
