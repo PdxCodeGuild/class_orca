1d00454542548df22570a303a85915d37e4826fc
eggxact_work <eggxact_work@eggxact.local>
2020-11-23 09:33:19 -0800

    update

---
+++ code/daniel/projects_django/chirp_project/chirp_posts/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import ChirpPosts
+
+admin.site.register(ChirpPosts) 
--- code/daniel/projects_django/url_shortner_project/url_shortner_app/models.py
+++ code/daniel/projects_django/url_shortner_project/url_shortner_app/models.py
@@ -5,9 +5,17 @@ class urlCode(models.Model):
     url_code = models.CharField(max_length=50)
     url_name = models.CharField(max_length=200)
     url = models.TextField()
+    url_counter = models.IntegerField(default=0)
 
     def __str__(self):
         return self.url_code
 
-
+class METAData(models.Model):
+    ip = models.CharField(max_length=200)
+    server_name = models.CharField(max_length=200)
+    server_port = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.ip
+    
     
