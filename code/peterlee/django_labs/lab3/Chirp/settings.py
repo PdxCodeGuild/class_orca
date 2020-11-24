8a287cede065be3047683a13e19276ea6508a8fc
unknown <plx1@protonmail.com>
2020-11-24 17:15:18 -0500

    lab3 update

--- code/peterlee/django_labs/lab3/Chirp/settings.py
+++ code/peterlee/django_labs/lab3/Chirp/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static']
+
+LOGIN_REDIRECT_URL = 'Posts:index'
+LOGOUT_REDIRECT_URL = 'Posts:index'
+
+LOGIN_URL = 'login'
