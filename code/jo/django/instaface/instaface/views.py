618905fd581aa3cc401189b5236d302730de1bc5
bit_jax <axel.omley@gmail.com>
2020-11-23 16:17:22 -0800

    lab3 update

--- code/jo/django/instaface/instaface/views.py
+++ code/jo/django/instaface/instaface/views.py
@@ -2,6 +2,10 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 from .models import Post
 
@@ -18,24 +22,36 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     template_name = 'post_new.html'
     fields = ['text', 'caption', 'photo', 'poster']
 
-    # def form_valid(self, form):
-    #     form.instance.author = self.request.user
-    #     return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.poster = self.request.user
+        return super().form_valid(form)
 
 class PostEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['text', 'caption']
 
-    # def test_func(self):
-    #     post = self.get_object()
-    #     return self.request.user == post.author
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.poster
         
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = 'post_delete.html'
+    template_name = 'delete_post.html'
     success_url = reverse_lazy('posts:home')
 
-    # def test_func(self):
-    #     obj = self.get_object()
-    #     return self.request.user == obj.author
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.poster
+
+class SignupView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+
+class UserProfileView(generic.DetailView):
+    model = User
+    template_name = 'user_profile.html'
+
+    def get_object(self):
+        return get_object_or_404(User, username=self.kwargs['username'])
--- code/jo/django/instaface/instaface_project/settings.py
+++ code/jo/django/instaface/instaface_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'instaface.apps.InstafaceConfig'
+    'instaface.apps.InstafaceConfig',
+    # 'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
