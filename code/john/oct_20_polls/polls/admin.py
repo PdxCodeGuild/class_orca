5ec6f3ad03a232391ad7a9b0108837ac6e707ac2
johnfial <12399190+johnfial@users.noreply.github.com>
2020-11-30 16:08:10 -0800

    javascript lab 1, conversion of rock paper scissors

--- code/john/oct_20_polls/polls/admin.py
+++
@@ -1,9 +0,0 @@
-# POLLS EXAMPLE PROJECT
-
-from django.contrib import admin
-
-# Register your models here.
-from .models import Choice, Question
-
-admin.site.register(Question)
-admin.site.register(Choice)
