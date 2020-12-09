53709697f300b94c32e0482a6c10ffaa1cfbacbf
eggxact_work <degg9407@gmail.com>
2020-12-09 09:07:01 -0800

    lab 4

--- code/daniel/JavaScript/toDoList/script.js
+++ code/daniel/JavaScript/toDoList/script.js
@@ -1,47 +1,107 @@
-let container = document.getElementById("container")
-let listForm = document.getElementById("item_generator")
-let completed_items = document.getElementById("completed_items")
-let list = document.createElement("div")
+// Global variables
+let item_generator = document.getElementById("item_generator")
+let todo_list = document.getElementById("todo_list")
+let completed_list = document.getElementById("completed_list")
 
-let add_item = document.createElement("input")
-add_item.setAttribute("type", "text")
-add_item.setAttribute("placeholder", " Add Item")
-container.appendChild(add_item)
+// User input
+let input_container = document.createElement("p")
+input_container.id = "add_item"
+let input_field = document.createElement("input")
+input_field.setAttribute("type", "text")
+input_field.setAttribute("placeholder", " Enter item")
+input_container.appendChild(input_field)
+item_generator.appendChild(input_container)
 
+// Submit button
+let submit_btn = document.createElement("p")
+submit_btn.id = "add_item"
+let submit_event = document.createElement("input")
+submit_event.setAttribute("type", "button")
+submit_event.setAttribute("value", "Submit")
+submit_btn.appendChild(submit_event)
+item_generator.appendChild(submit_btn)
+input_field.onkeypress = function (e) {
+    if (e.key === "Enter") {
+        add_item()
+    }
+}
+submit_event.onclick = add_item
 
+// Additem to Todo List
+function add_item () {
 
-let submit = document.createElement("input")
-submit.setAttribute("type", "button")
-submit.setAttribute("value", "Submit")
-container.appendChild(submit)
-
-submit.addEventListener('click', function (event) {
-    
-    let item_bucket = document.createElement("div")
+    // New div for added item
+    let todo_item = document.createElement("div")
+    todo_item.className = "item"
     let item = document.createElement("p")
-    item_bucket.id = add_item.value
-    let complete = document.createElement("input")
-    complete.setAttribute("type", "button")
-    complete.setAttribute("value", "COMPLETE")
-    complete.addEventListener('click', function () {
-        item.style.textDecorationLine = "line-through"
-        completed_items.appendChild(item)
-        completed_items.appendChild(delete_item)
-        item_bucket.remove()
-    })
-    let delete_item = document.createElement("input")
-    delete_item.setAttribute("type", "button")
-    delete_item.setAttribute("value", "DELETE")
-    delete_item.addEventListener("click", function () {
-        item.remove()
-        delete_item.remove()
-        item_bucket.remove()
+    item.innerText = input_field.value
+    let created_on = document.createElement("p")
+    created_on.innerText = "created on: "+todays_date()
 
-    })
-    item.innerText = add_item.value
-    item_bucket.appendChild(complete)
-    item_bucket.appendChild(delete_item)
-    item_bucket.appendChild(item)
-    listForm.appendChild(item_bucket)
-    add_item.value=''
-})
+    // Delete button
+    let delete_btn = document.createElement("p")
+    let delete_event = document.createElement("input")
+    delete_event.setAttribute("type", "button")
+    delete_event.setAttribute("value", "Delete")
+    delete_btn.appendChild(delete_event) 
+    delete_event.onclick = function () {
+        todo_list.removeChild(todo_item)
+    } 
+
+    // Complete button
+    let complete_btn = document.createElement("p")
+    let complete_event = document.createElement("input")
+    complete_event.setAttribute("type", "button")
+    complete_event.setAttribute("value", "Complete")
+    complete_btn.appendChild(complete_event)
+    complete_event.onclick = function () {
+        complete_item(created_on, todo_item, complete_btn, delete_btn)
+    }
+
+    // Append elements
+    todo_item.appendChild(item)
+    todo_item.appendChild(created_on)
+    todo_item.appendChild(complete_btn)
+    todo_item.appendChild(delete_btn) 
+    todo_list.appendChild(todo_item)
+    input_field.value = ''
+} 
+
+// Complete an item
+function complete_item (created_on, todo_item, complete_btn, delete_btn) {
+    let completed_on = document.createElement("p")
+    completed_on.innerText = "completed on: "+todays_date()
+    todo_item.appendChild(completed_on)
+    completed_list.appendChild(todo_item)
+    todo_item.removeChild(complete_btn, delete_btn)
+    
+    let add_again_btn = document.createElement("p")
+    let add_again_event = document.createElement("input")
+    add_again_event.setAttribute("type", "button")
+    add_again_event.setAttribute("value", "Add Again")
+    add_again_btn.appendChild(add_again_event)
+    todo_item.appendChild(add_again_btn)
+    add_again_btn.onclick = function () {
+        todo_item.removeChild(completed_on)
+        created_on.innerText = "created on: "+todays_date()
+        add_again_btn.remove()
+        delete_btn.remove()
+        todo_item.appendChild(complete_btn)
+        todo_item.appendChild(delete_btn)
+        todo_list.appendChild(todo_item)
+    }
+    todo_item.appendChild(delete_btn)
+    delete_btn.onclick = function () {
+        completed_list.removeChild(todo_item)
+    }
+}
+
+// Today's date
+function todays_date () {
+    let today = new Date()
+    let day = today.getDate()
+    let month = today.getMonth()+1
+    let year = today.getFullYear()
+    let date = document.createElement("p")
+    return (`${month}/${day}/${year}`)
+}
