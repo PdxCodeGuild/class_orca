46f88a3c06248020a4f3f332c256cc54f5090868
a-garey <agarey@uchicago.edu>
2020-12-11 18:27:02 -0600

    lab11

--- code/austin/4-JavaScript/Lab11/js/site.js
+++ code/austin/4-JavaScript/Lab11/js/site.js
@@ -2,12 +2,22 @@
 let vm = new Vue({
     el: '#app', 
     data: {
+        task: "",
         tasks: ['Make egg nog'],
-        complete: false,
+        doneTasks: [],
+        isComplete: false
     },
     methods: {
         addTask: function() {
-            this.tasks.push("new task")
+            this.tasks.push(this.task)
+            this.task = " "
+        },
+        markDone: function(task, index) {
+            this.doneTasks.push(task)
+            this.tasks.splice(index, 1)
+        },
+        deleteTask: function(index) {
+            this.tasks.splice(index, 1)
         },
     } 
 });
