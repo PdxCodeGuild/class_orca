dfa6f8843d602a22ddab2d8d204a74a0d2bb8453
Merritt Lawrenson <merrittlawrenson@gmail.com>
2020-12-14 15:10:40 -0800

    components

--- code/merritt/oct-20-vue-todos/main.js
+++ code/merritt/oct-20-vue-todos/main.js
@@ -1,34 +1,58 @@
 Vue.component('todo-item', {
     data: function() {
         return {
-
+            editing: false
         }
     },
     props: ['todo'],
     template: `
     <li>
         <input type="checkbox" v-model="todo.completed">
-        {{ todo.text }}
-        <button @click="removeTodo(todo, $event)">×</button>
+        <input v-if="editing" type="text" v-model="todo.text" @keyup.enter="toggleEdit">
+        <template v-else>{{ todo.text }}</template>
+        <button @click="toggleEdit">{{ editing ? "Done" : "Edit" }}</button>
+        <button @click="$emit('remove', todo)">×</button>
     </li>
-    `
+    `,
+    methods: {
+        toggleEdit: function() {
+            this.editing = this.editing ? false : true
+        }
+    }
+})
+
+Vue.component('add-todo', {
+    data: function() {
+        return {
+            text: "",
+            id: 1
+        }
+    },
+    template: `
+    <div>
+        <input type="text" placeholder="What do you want to do?" @keyup.enter="addTodo" v-model="text">
+        <button @click="addTodo">Add to List</button>
+    </div>
+    `,
+    methods: {
+        addTodo: function() {
+            this.$emit('add', { text: this.text, completed: false, id: this.id })
+            this.id++
+            this.text = ""
+        }
+    }
 })
 
 let vm = new Vue({
     el: "#app",
     data: {
         todos: [],
-        newTodoText: "",
-        newTodoId: 1
     },
     methods: {
-        addTodo: function() {
-            this.todos.push({ text: this.newTodoText, completed: false, id: this.newTodoId })
-            this.newTodoId++
-            this.newTodoText = ""
+        addTodo: function(todo) {
+            this.todos.push(todo)
         },
-        removeTodo: function(todo, event) {
-            console.log(event)
+        removeTodo: function(todo) {
             this.todos.splice(this.todos.indexOf(todo), 1)
         }
     },
