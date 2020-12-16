7d85ea0cf6d75d2c3d784ddd1590a4d350fd897e
sjohnston82 <stephen.johnston1@yahoo.com>
2020-12-16 11:42:50 -0800

    lab13

--- code/stephen/Javascript/Lab13/main.js
+++ code/stephen/Javascript/Lab13/main.js
@@ -1,3 +1,4 @@
+
 let vm = new Vue({
     el: '#app',
     data: {
@@ -7,7 +8,9 @@ let vm = new Vue({
         searchType: '',
         searching: false,
         page: 1,
-        lastpage: false
+        lastpage: false,
+        count: 0,
+        
     },
     methods: {
         loadQuotes: function() {
@@ -23,19 +26,69 @@ let vm = new Vue({
                     page: this.page
                 }
             }).then(response => {
+                // if (response.data.last_page = true){
+                //     this.lastpage = true
+                //     console.log(this.lastpage)
+                // } else {
+                //     this.lastpage = false
+                // }
                 this.quotes = response.data.quotes
                 this.searching = true
-                console.log(response.data)
+                this.page =  response.data.page
+                this.lastpage = response.data.last_page
+            
+                // console.log(response.data.count)
             })
         },
+        pageChange: function() {
+            axios({
+                url: "https://favqs.com/api/quotes/",
+                method: 'GET',
+                headers: {
+                    "Authorization": `Token token="${apiKey}"`
+                },
+                params: {
+                    filter: this.searchTerm,
+                    type: this.searchType,
+                    page: this.page,
+                    
+                    
+                }
+            }).then(response => {
+                this.quotes = response.data.quotes
+                this.searching = true
+                this.page =  response.data.page
+                this.lastpage = response.data.last_page
+            })
+        },
+        // pageChange: function() {
+        //     axios({
+        //         url: "https://favqs.com/api/typehead/",
+        //         method: 'GET',
+        //         headers: {
+        //             "Authorization": `Token token="${apiKey}"`
+        //         },
+        //         params: {
+        //             count: this.quotes.length
+        //         }
+        //     }).then(response => {
+        //         this.quotes = response.data.quotes
+        //         this.searching = true
+                
+            
+        //         console.log(response.data)
+        //     })
+        // },
         nextPage: function() {
             this.page = this.page+=1
-            console.log(this.page)
-            this.loadQuotes()
+            // console.log(this.page)
+            this.pageChange()
+            window.scrollTo({ top: 0, behavior: 'smooth' })
         },
         previousPage: function() {
             this.page = this.page-=1
-            this.loadQuotes()
+            this.pageChange()
+            window.scrollTo({ top: 0, behavior: 'smooth' })
         }
     },
     mounted: function() {
