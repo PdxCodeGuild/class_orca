f328c3a31e09fb482e1e7d3fa17bc5eda4d9c938
unknown <plx1@protonmail.com>
2020-12-17 18:43:37 -0500

    lab14 update

---
+++ code/peterlee/js_labs/lab14/main.js
@@ -0,0 +1,28 @@
+const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=${apiKey}';
+
+requestFile( url );
+
+
+function requestFile( url ) {
+
+    const xhr = new XMLHttpRequest();
+    xhr.open( 'GET', url, true );
+    xhr.onerror = function( xhr ) { console.log( 'error:', xhr  ); };
+    xhr.onprogress = function( xhr ) { console.log( 'bytes loaded:', xhr.loaded  ); };
+    xhr.onload = callback;
+    xhr.send( null );
+
+    function callback( xhr ) {
+
+        let response, json, lines;
+
+        response = xhr.target.response;
+        app.innerText = response;
+
+        json = JSON.parse( response );
+
+
+
+    }
+
+}
