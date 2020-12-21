18c5cd643f16505b2a8de5768eb266d30fed3ba0
Merritt Lawrenson <merrittlawrenson@gmail.com>
2020-12-21 14:56:49 -0800

    added vue frontend

--- code/merritt/oct-20-drf-blog/api/urls.py
+++ code/merritt/oct-20-drf-blog/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import PostList, PostDetail
 
 urlpatterns = [
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
+    path('posts/<int:pk>/', PostDetail.as_view()),
+    path('posts/', PostList.as_view()),
 ]
