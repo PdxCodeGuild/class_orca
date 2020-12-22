4f2d3630813beb054222a050e6997850f98cd1bf
johnfial <12399190+johnfial@users.noreply.github.com>
2020-12-22 15:35:33 -0800

    django lab06 OK but not great, lame auth (in html template render only)

---
+++ code/john/django_labs/django_lab_6_dec21_REST_framework_and_VUE/api_app/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from students.models import Student
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ('first_name', 'last_name', 'cohort', 'favorite_topic', 'favorite_teacher', 'capstone')
+
+    
+# merrit here uses a 'nested serializer'
