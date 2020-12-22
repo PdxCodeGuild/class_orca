397c0e995370b2e8bd4b604477d31ba89a73b0fa
mansolil <ron.mansolilli@gmail.com>
2020-12-22 14:58:40 -0800

    update

--- code/ron/django/lab6/students_project/urls.py
+++ code/ron/django/lab6/students_project/urls.py
@@ -1,7 +1,7 @@
-"""library_project URL Configuration
+"""students_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('', include())
     path('admin/', admin.site.urls),
+    path('user/', include('django.contrib.auth.urls')),
+    path('user/', include('users.urls')),
+    path('', include('students.urls')),
+    path('api/', include('api.urls')),
 ]
